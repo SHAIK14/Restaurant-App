688c262bff73b3063697a84af812c667f31ea9ab
shaik Asif <smd.20sa@gmail.com>
2023-03-02 18:17:18 +0530

    add the functionality to +  and  - of the showcart button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,30 @@
 import { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 const Cart = (props) => {
   const cartctx = useContext(CartContext);
   const totalAmount = `$${cartctx.totalAmount.toFixed(2)}`;
   const hasItems = cartctx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    cartctx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartctx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartctx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          Key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
